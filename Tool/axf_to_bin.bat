d36f047a409ebd53472194d6cbee77161baf597d
Cat <843553493@qq.com>
2018-10-26 15:02:14 +0800

    Update axf_to_bin.bat

--- Tool/axf_to_bin.bat
+++ Tool/axf_to_bin.bat
@@ -1,13 +1,20 @@
+@echo off
+
 REM set fromelf.exe directory
 set FROMELF=C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe
 
 REM check fromelf.exe existence
-if exist %FROMELF% (echo fromelf.exe ready) else (echo fromelf.exe directory error & pause & exit)
+if exist %FROMELF% (goto STEP2) else (goto STEP1)
 
+:STEP1
+echo fromelf.exe directory error
+pause & exit
+
+:STEP2
 REM if .\..\Output\STM32F103_CLI.axf exit, then convert it to bin
 if exist .\..\Output\STM32F103_CLI.axf (%FROMELF% ".\..\Output\STM32F103_CLI.axf" --bin --output ".\..\Output\STM32F103_CLI.bin")
 
 REM if STM32F103_CLI.bin convert success, then copy the bin file to this dir.
 if exist .\..\Output\STM32F103_CLI.bin (copy .\..\Output\STM32F103_CLI.bin .\)
 
-pause
+pause & exit
