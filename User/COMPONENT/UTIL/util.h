b75e45a8c682d982f15883e6c4554069e182976a
Cat <sharecat73@gmail.com>
2019-03-05 11:13:35 +0800

    Update util.h

--- User/COMPONENT/UTIL/util.h
+++ User/COMPONENT/UTIL/util.h
@@ -3,6 +3,17 @@
 #ifndef __APP_UTIL_H
 #define __APP_UTIL_H
 
+#include <stdint.h>
+
+
+/* 大小端转换宏 */
+#define BIG_LITTLE_SWAP16(A)        ((((uint16_t)(A) & 0xff00) >> 8)        \
+                                    | (((uint16_t)(A) & 0x00ff) << 8))
+
+#define BIG_LITTLE_SWAP32(A)        ((((uint32_t)(A) & 0xff000000) >> 24)   \
+                                    | (((uint32_t)(A) & 0x00ff0000) >> 8)   \
+                                    | (((uint32_t)(A) & 0x0000ff00) << 8)   \
+                                    | (((uint32_t)(A) & 0x000000ff) << 24))
 
 
 extern int my_atoi(char *str);
