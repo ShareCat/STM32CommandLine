4068de0205caac305e14aa8ca22ff2923e3285fa
Cat <843553493@qq.com>
2021-09-13 17:37:48 +0800

    Update util.h

--- User/COMPONENT/UTIL/util.h
+++ User/COMPONENT/UTIL/util.h
@@ -16,6 +16,7 @@
                                     | (((uint32_t)(A) & 0x000000ff) << 24))
 
 
+extern int str_is_digit(char *p_str);
 extern void my_delete_char(char str[], char target);
 extern void my_str_remove_blank(char *str);
 extern void my_hex_to_str(unsigned char *pHex, unsigned char *pAscii, int nLen);
